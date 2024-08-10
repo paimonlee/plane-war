3da8daa307b4500cd265550b6f2e12796b9c1e4c
paimonlee <2253339528@qq.com>
2024-08-10 23:37:17 +0800

    实现动态加载战机图片

--- assets/scripts/Me.ts
+++ assets/scripts/Me.ts
@@ -1,14 +1,52 @@
-import { _decorator, Component, EventTouch, Input, NodeEventType, UITransform, Vec3, view } from 'cc';
+import { _decorator, Component, EventTouch, ImageAsset, Input, log, NodeEventType, resources, sp, Sprite, SpriteFrame, Texture2D, UITransform, Vec3, view } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Me')
 export class Me extends Component {
+    readonly me1: string = "images/me1/spriteFrame"
+
+    readonly me2: string = "images/me2/spriteFrame"
+
+    readonly loadMeInterval = 0.5;
+
     private touchDown: boolean;
 
-    viewHeight: number = view.getVisibleSize().height;
-    viewWidth: number = view.getVisibleSize().width;
+    private viewHeight: number = view.getVisibleSize().height;
+
+    private viewWidth: number = view.getVisibleSize().width;
+
+    private currMe = 1;
+
+    private loadMeWait: number = 0;
 
     start() {
+        this.registeEvent()
+    }
+
+    update(deltaTime: number) {
+        this.loadMeWait += deltaTime;
+        if (this.loadMeWait >= this.loadMeInterval) {
+            if (this.currMe == 1) {
+                this.loadMe(this.me2);
+                this.currMe = 2;
+            } else {
+                this.loadMe(this.me1);
+                this.currMe = 1;
+            }
+            this.loadMeWait = 0;
+        }
+    }
+
+    private loadMe(url: string) {
+        resources.load(url, (err, frame: SpriteFrame) => {
+            if (err) {
+                log("load me failed.%s", err)
+            }
+            this.node.getComponent(Sprite).spriteFrame = frame;
+        })
+    }
+
+    registeEvent() {
         this.node.on(Input.EventType.TOUCH_START, (event: EventTouch) => {
             this.touchDown = true;
         }, this);
@@ -28,10 +66,6 @@ export class Me extends Component {
         this.node.on(Input.EventType.TOUCH_END, (event: EventTouch) => {
             this.touchDown = false;
         }, this);
-    }
-
-    update(deltaTime: number) {
-
     }
 }
 
